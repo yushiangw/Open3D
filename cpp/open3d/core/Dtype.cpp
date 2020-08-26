73ab43dfe379e058ac998e40a5c35e253d3decd4
Yixing Lao <yixing.lao@gmail.com>
2020-08-26 03:57:36 -0700

    move code

--- cpp/open3d/core/Dtype.cpp
+++ cpp/open3d/core/Dtype.cpp
@@ -30,20 +30,25 @@
 namespace open3d {
 namespace core {
 
-static_assert(sizeof(float) == 4,
-              "Unsupported platform: float must be 4 bytes.");
-static_assert(sizeof(double) == 8,
-              "Unsupported platform: double must be 8 bytes.");
-static_assert(sizeof(int) == 4, "Unsupported platform: int must be 4 bytes.");
-static_assert(sizeof(int32_t) == 4,
-              "Unsupported platform: int32_t must be 4 bytes.");
-static_assert(sizeof(int64_t) == 8,
-              "Unsupported platform: int64_t must be 8 bytes.");
-static_assert(sizeof(uint8_t) == 1,
-              "Unsupported platform: uint8_t must be 1 byte.");
-static_assert(sizeof(uint16_t) == 2,
-              "Unsupported platform: uint16_t must be 2 bytes.");
-static_assert(sizeof(bool) == 1, "Unsupported platform: bool must be 1 byte.");
+// clang-format off
+static_assert(sizeof(float)    == 4, "Unsupported platform: float must be 4 bytes."   );
+static_assert(sizeof(double)   == 8, "Unsupported platform: double must be 8 bytes."  );
+static_assert(sizeof(int)      == 4, "Unsupported platform: int must be 4 bytes."     );
+static_assert(sizeof(int32_t)  == 4, "Unsupported platform: int32_t must be 4 bytes." );
+static_assert(sizeof(int64_t)  == 8, "Unsupported platform: int64_t must be 8 bytes." );
+static_assert(sizeof(uint8_t)  == 1, "Unsupported platform: uint8_t must be 1 byte."  );
+static_assert(sizeof(uint16_t) == 2, "Unsupported platform: uint16_t must be 2 bytes.");
+static_assert(sizeof(bool)     == 1, "Unsupported platform: bool must be 1 byte."     );
+
+const Dtype Dtype::Undefined(Dtype::DtypeCode::Undefined, 1, "Undefined");
+const Dtype Dtype::Float32  (Dtype::DtypeCode::Float,     4, "Float32"  );
+const Dtype Dtype::Float64  (Dtype::DtypeCode::Float,     8, "Float64"  );
+const Dtype Dtype::Int32    (Dtype::DtypeCode::Int,       4, "Int32"    );
+const Dtype Dtype::Int64    (Dtype::DtypeCode::Int,       8, "Int64"    );
+const Dtype Dtype::UInt8    (Dtype::DtypeCode::UInt,      1, "UInt8"    );
+const Dtype Dtype::UInt16   (Dtype::DtypeCode::UInt,      2, "UInt16"   );
+const Dtype Dtype::Bool     (Dtype::DtypeCode::Bool,      1, "Bool"     );
+// clang-format on
 
 Dtype::Dtype(DtypeCode dtype_code, int64_t byte_size, const std::string &name)
     : dtype_code_(dtype_code), byte_size_(byte_size) {
@@ -63,16 +68,7 @@ bool Dtype::operator==(const Dtype &other) const {
 
 bool Dtype::operator!=(const Dtype &other) const { return !(*this == other); }
 
-// clang-format off
-const Dtype Dtype::Undefined(Dtype::DtypeCode::Undefined, 1, "Undefined");
-const Dtype Dtype::Float32  (Dtype::DtypeCode::Float,     4, "Float32"  );
-const Dtype Dtype::Float64  (Dtype::DtypeCode::Float,     8, "Float64"  );
-const Dtype Dtype::Int32    (Dtype::DtypeCode::Int,       4, "Int32"    );
-const Dtype Dtype::Int64    (Dtype::DtypeCode::Int,       8, "Int64"    );
-const Dtype Dtype::UInt8    (Dtype::DtypeCode::UInt,      1, "UInt8"    );
-const Dtype Dtype::UInt16   (Dtype::DtypeCode::UInt,      2, "UInt16"   );
-const Dtype Dtype::Bool     (Dtype::DtypeCode::Bool,      1, "Bool"     );
-// clang-format on
+
 
 }  // namespace core
 }  // namespace open3d
